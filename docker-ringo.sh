4963594c6bb1751477a30cd2c447d70d44c36e27
Torsten Irländer <torsten.irlaender@googlemail.com>
2016-12-23 13:42:45 +0100

    Added shell command.

--- docker-ringo.sh
+++ docker-ringo.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-set -x
+#set -x
 
 CMD=$1
-APPNAME=$2
+APPNAME=${2-`basename $PWD`}
 
 DBCMD="docker run --name ringo-postgres -e POSTGRES_USER=ringo -e POSTGRES_PASSWORD=ringo -d postgres"
-RCMD="docker run --name  $APPNAME -t -i --link ringo-postgres -v $PWD:/ringo/src -p 6543:6543 toirl/ringo"
-SRCMD="docker run --rm --name  $APPNAME -t -i --link ringo-postgres -v $PWD:/ringo/src -p 6543:6543 toirl/ringo"
+RCMD="docker run --name  $APPNAME -it --link ringo-postgres -v $PWD:/ringo/src -p 6543:6543 toirl/ringo"
+SRCMD="docker run --rm --name $APPNAME -it --link ringo-postgres -v $PWD:/ringo/src -p 6543:6543 toirl/ringo"
+SHELL="docker run --name $APPNAME -it --entrypoint=/bin/bash --link ringo-postgres -v $PWD:/ringo/src -p 6543:6543 toirl/ringo"
 
 # Check if there is a container named "ringo-postgres" running. If not start
 # it.
@@ -16,6 +17,8 @@ if [ $? -gt 0 ]; then
   echo $?
   echo "Starting new postgres container"
   $DBCMD
+else
+  echo "DB already running."
 fi
 
 case $CMD in
@@ -30,4 +33,9 @@ case $CMD in
   init)
     echo "(Re)init database"
     $SRCMD init $APPNAME
+    ;;
+  shell)
+    echo "Open Shell"
+    $SHELL
+    ;;
 esac
